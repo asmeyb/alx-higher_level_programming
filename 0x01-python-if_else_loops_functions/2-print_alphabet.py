8350ba2b9b4cfb9a28f5b5c23f5530ec888e230f
asmeyb <asmeb@gmail.com>
2021-09-07 06:10:26 -0700

    Lower Case print in new line

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-print("".format()
-        .join([chr(i) for i in range(ord('a'), ord('z') + 1)]), end="")
+print("".format().join([chr(i) for i in range(ord('a'), ord('z') + 1)]), end="")
