55819a1006ea273079abc11f7d52db261d27a211
asmeyb <asmeb@gmail.com>
2021-09-09 03:52:12 -0700

    Edit on Hidden Discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
     import hidden_4
-    for n in dir(hidden_4):
-        if n[:2] != "__":
-            print(n)
+
+    for i in dir(hidden_4):
+        if i[:2] == "__":
+            continue
+        print(i)
