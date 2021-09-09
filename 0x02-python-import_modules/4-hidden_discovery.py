986451820ff2d9173f4fb38435e1ea1663ee6342
asmeyb <asmeb@gmail.com>
2021-09-09 07:59:44 -0700

    Update on Hidden Discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    all_dir = dir(hidden_4)
-    avoid = "__"
-    for i in range(0, len(all_dir)):
-        if avoid not in all_dir[i]:
-            print(all_dir[i])
+    for name in dir(hidden_4):
+        if name.startswith('__'):
+            continue
+        print("{:s}".format(name))
