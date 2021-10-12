98fc7c4ab6aef4594a99f00cdb04260a35ca37be
asmeyb <asmeb@gmail.com>
2021-10-12 03:26:40 -0700

    1 & 2 With main.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module for write_file
+"""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a text file"""
+    with open(filename, 'w') as f:
+        c = f.write(text)
+    return c
