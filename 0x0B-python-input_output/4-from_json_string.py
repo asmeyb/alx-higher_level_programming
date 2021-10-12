f6d5602acfb7e243e72c1c33ac83f39972d6878b
asmeyb <asmeb@gmail.com>
2021-10-12 03:47:25 -0700

    From JSON and 4-main.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module for from_json_string.
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """Return an object (Python data structure)"""
+    return json.loads(my_str)
