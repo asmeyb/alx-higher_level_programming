9b6932127348a47911b7d38162770b8477f31777
asmeyb <asmeb@gmail.com>
2021-10-15 13:23:30 -0700

    Adding __init__ & base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+module for Base class
+"""
+
+import json
+import os
+import csv
+
+
+class Base:
+    """A base"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Intialize a base."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
