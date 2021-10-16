e3a4c9bd063f70ac3a31be63b7c4d156084412ad
asmeyb <asmeb@gmail.com>
2021-10-16 01:35:07 -0700

    All Done(Base) in here

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,66 @@ class Base:
         if any(type(i) != dict for i in list_dictionaries):
             raise TypeError("list_dictionaries must contain dictionaries")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """instance"""
+        aux = cls(1, 1) if cls.__name__ == "Rectangle" else cls(1)
+        aux.update(**dictionary)
+        return aux
+
+    @classmethod
+    def load_from_file(cls):
+        """lisr of instances"""
+        filename = str(cls).split(".")[-1][:-2] + ".json"
+        if not os.path.exists(filename):
+            return []
+        ret = []
+        with open(filename, "r") as f:
+            dictionary = cls.from_json_string(f.readline())
+        for x in dictionary:
+            ret.append(cls.create(**x))
+        return ret
+
+    @classmethod
+    def save_to_file_csv(cls, data):
+        """CSV"""
+        if type(data) != list and data is not None:
+            raise TypeError("list_objs must be a list")
+        if not all(isinstance(i, cls) for i in data):
+            raise TypeError("list_objs must be a list")
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w") as f:
+            if data is not None:
+                data = [i.to_dictionary() for i in data]
+                sfields = ['id', 'size', 'x', 'y']
+                rfields = ['id', 'width', 'height', 'x', 'y']
+                if cls.__name__ == "Rectangle":
+                    writer = csv.DictWriter(f, fieldnames=rfields)
+                else:
+                    writer = csv.DictWriter(f, fieldnames=sfields)
+                writer.writeheader()
+                writer.writerows(data)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """CSV"""
+        filename = cls.__name__ + ".csv"
+        sheader = ["id", "size", "x", "y"]
+        rheader = ["id", "width", "height", "x", "y"]
+        if cls.__name__ == "Rectangle":
+            header = rheader
+        else:
+            header = sheader
+        result = []
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
+                reader = csv.reader(f, delimiter=',')
+                for i, row in enumerate(reader):
+                    if i > 0:
+                        new = cls(1, 1)
+                        for x, y in enumerate(row):
+                            if y:
+                                setattr(new, header[x], int(y))
+                        result.append(new)
+        return result
