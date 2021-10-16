24e6def11b29e6c422f12b8145335ee2f118ab8f
asmeyb <asmeb@gmail.com>
2021-10-16 01:13:37 -0700

    Q9 __init__, __str__, sizeSetter

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+module for Square class.
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ string"""
+        y = str(self.y)
+        x = str(self.x)
+        w = str(self.width)
+        i = str(self.id)
+        string = "[Square] (" + i + ") " + x + "/" + y + " - " + w
+        return string
+
+    @property
+    def size(self):
+        """Getter"""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """Setter"""
+        self.width = size
+        self.height = size
