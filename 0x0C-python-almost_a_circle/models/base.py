fa618e794b436c9ec8b761eff724ae9e53d8ece6
asmeyb <asmeb@gmail.com>
2021-10-16 01:51:13 -0700

    IImplmentation from 14-main to 18-main & update on model/base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 module for Base class
 """
@@ -31,6 +32,35 @@ class Base:
         if any(type(i) != dict for i in list_dictionaries):
             raise TypeError("list_dictionaries must contain dictionaries")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, tosave):
+        """Write"""
+        if type(tosave) != list and tosave is not None:
+            raise TypeError("list_objs must be a list")
+        if tosave is None or tosave == []:
+            out = []
+        else:
+            first = type(tosave[0])
+            if any(type(i) != first for i in tosave):
+                raise ValueError("all elements of list_objs must match")
+            out = [i.to_dictionary() for i in tosave]
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(out))
+
+    @staticmethod
+    def from_json_string(string):
+        """return from json a string"""
+        if string is None or string == "":
+            return []
+        if type(string) != str:
+            raise TypeError("json_string must be a string")
+        data = json.loads(string)
+        for d in data:
+            if type(d) != dict:
+                raise ValueError("json_string must contain dictionaries")
+        return data
 
     @classmethod
     def create(cls, **dictionary):
