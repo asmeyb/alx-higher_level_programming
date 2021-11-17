871e54a8b0822f23d747e3eb631c78071c949072
asmeyb <asmeb@gmail.com>
2021-11-17 10:16:45 -0800

    Count, AS and Join

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
+GROUP BY tv_genres.id
 ORDER BY number_shows DESC;
