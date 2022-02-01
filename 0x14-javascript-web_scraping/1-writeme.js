4df969fad0b041e375577b671cb12d496c580711
root <Asmamaw Yismaw>
2022-02-01 02:16:42 -0800

    Write to File

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const filePath = process.argv[2];
+const writeString = process.argv[3];
 
-fs.writeFile(`${process.argv[2]}`,`${process.argv[3]}`,'utf8', (err) => {
-    if(err){
-	console.log(err);
-    }
+fs.writeFile(filePath, writeString, (err) => {
+  if (err) {
+    return console.error(err);
+  }
 });
