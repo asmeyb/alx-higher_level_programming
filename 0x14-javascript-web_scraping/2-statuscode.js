6a70ac8355fa8d6b11d7755a96cc436b019aea39
root <Asmamaw Yismaw>
2022-02-01 02:21:18 -0800

    Status Code for a Request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
 
-request(process.argv[2], (err, res) => {
-    if(err){
-	console.log(err);
-    }
-    else{
-	console.log(`code: ${res.statusCode}`);
-    }    
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
