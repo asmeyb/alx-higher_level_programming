bf0713728fb843436203654bc5f753b307a80f74
root <Asmamaw Yismaw>
2022-02-01 02:35:54 -0800

    Movie Characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,22 +1,15 @@
 #!/usr/bin/node
-
+/* Task 100 - script that prints all characters of a Star Wars movie */
 const request = require('request');
-
-request(`https://swapi-api.hbtn.io//api/films/${process.argv[2]}`, (err, res, body) => {
-    if(err){
-	console.log(err);
-    }
-    else{
-	const characters = JSON.parse(body).characters;
-	characters.forEach(character => {
-	    request(character, (err, res, body) => {
-		if(err){
-		    console.log(err);
-		}
-		else{
-		    console.log(JSON.parse(body).name);
-		}
-	    });
-	});
-    }
+/* The first argument is the Movie ID - example: 3 = "Return of the Jedi */
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) throw error;
+  for (const character of JSON.parse(body).characters) {
+    request(character, function (error, response, body) {
+      if (error) throw error;
+      /* Display one character name by line */
+      console.log(JSON.parse(body).name);
+    });
+  }
 });
