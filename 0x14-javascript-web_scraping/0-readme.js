7a8ca1fff57eadc409447ebb4c210a134dbf05d0
root <Asmamaw Yismaw>
2022-02-01 02:12:08 -0800

    Read File

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 
-const fs = require('fs')
+const fs = require('fs');
+const file = process.argv[2];
 
-fs.readFile(`${process.argv[2]}`, (err,data) => {
-    if(err){
-	console.log(err);
-    }
-    else{
-	console.log(data.toString('utf8'));
-    }
-});
+fs.readFile(file, 'utf8', error);
+
+function error (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+}
